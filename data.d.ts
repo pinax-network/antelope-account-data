89a45e4217aa2137b44a3fa4297f37cda85db656
Denis <carriere.denis@gmail.com>
2019-01-21 21:58:12 -0500

    Add exchanges & Dapps to builder

--- data.d.ts
+++ data.d.ts
@@ -1,9 +1,27 @@
 export type Dataset = string[];
 
+export interface Dapps {
+    "account-creation": Dataset;
+    collectibles: Dataset;
+    gambling: Dataset;
+    games: Dataset;
+    marketplaces: Dataset;
+    namebid: Dataset;
+    resources: Dataset;
+    social: Dataset;
+    "token-distribution": Dataset;
+}
+
+export interface Exchanges {
+    cex: Dataset;
+    dex: Dataset;
+}
+
+// Complex Datasets
+export const exchange: Exchanges;
+export const dapps: Dapps;
+
+// Simple Datasets
 export const blacklist: Dataset;
-export const dapps: Dataset;
-export const dex: Dataset;
-export const exchange: Dataset;
-export const gambling: Dataset;
 export const genesis: Dataset;
 export const system: Dataset;
--- scripts/build.js
+++ scripts/build.js
@@ -3,14 +3,41 @@ const load = require("load-json-file");
 const write = require("write-json-file");
 const glob = require("glob");
 
-const data = {};
+const data = {
+    exchanges: {},
+    dapps: {}
+};
 
+// Simple
 for (const filepath of glob.sync(path.join(__dirname, "..", "json", "*.json"))) {
-    const {name, ext} = path.parse(filepath);
-    if (ext !== ".json") continue;
+    const {name} = path.parse(filepath);
 
     const dataset = load.sync(filepath);
     data[name] = dataset;
+}
+
+// Dapps
+for (const filepath of glob.sync(path.join(__dirname, "..", "json", "dapps", "**", "*.json"))) {
+    const dataset = load.sync(filepath);
+    const sub = path.parse(filepath.split(path.join("json", "dapps"))[1]);
+    const name = path.parse(sub.dir).name;
+
+    if (!data.dapps[name]) data.dapps[name] = [];
+    for (const row of dataset) {
+        data.dapps[name].push(row);
+    }
+}
+
+// Exchanges
+for (const filepath of glob.sync(path.join(__dirname, "..", "json", "exchanges", "**", "*.json"))) {
+    const dataset = load.sync(filepath);
+    const sub = path.parse(filepath.split(path.join("json", "exchanges"))[1]);
+    const name = path.parse(sub.dir).name;
+
+    if (!data.exchanges[name]) data.exchanges[name] = [];
+    for (const row of dataset) {
+        data.exchanges[name].push(row);
+    }
 }
 
 write.sync(path.join(__dirname, "..", "data.json"), data);
