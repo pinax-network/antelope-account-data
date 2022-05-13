a92d44bdc44acf87f57ab3a522088bb778ecec44
Denis Carriere <carriere.denis@gmail.com>
2022-05-12 22:10:43 -0400

    update coinbase

--- scripts/parse-detective.ts
+++ scripts/parse-detective.ts
@@ -1,7 +1,7 @@
-const huobi = require("./zb.json")
+const { data }  = require("./coinbase.json")
 
 const accounts = new Set();
-for ( const row of huobi.data ) {
+for ( const row of data ) {
     const from = row.from.split("/")[1];
     const to = row.to.split("/")[1];
     accounts.add(from);
